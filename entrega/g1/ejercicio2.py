ec655d7b627465e18eca9fb06118b4f96c9e38d3
caroito <161514036+caroito@users.noreply.github.com>
2024-03-02 18:56:53 +0000

    ejercicio3 A-I

--- entrega/g1/ejercicio2.py
+++
@@ -1,7 +0,0 @@
-print(6/3)
-print(6//3)
-print(6%3)
-print(2**2)
-print(type("hola"))
-print(type(40))
-
---
+++ entrega/g1/ejercicio3.py
@@ -0,0 +1,26 @@
+nombre_alumno = "Carolina"
+print(nombre_alumno)
+
+apellido_alumno = "Andrade"
+print(apellido_alumno)
+
+edad = 19
+print(edad)
+
+altura = "1,60"
+print("mido", altura)
+
+num_vuelo = "AD8"
+print(num_vuelo)
+
+temperatura = "19,8"
+print("la temperatura es", temperatura,"grados")
+
+salario = 200000
+print("mi salario mensual tiene un monto de", salario)
+
+game_over = "juego terminado"
+print(game_over)
+
+num_par = "tu número es par"
+print(num_par)
