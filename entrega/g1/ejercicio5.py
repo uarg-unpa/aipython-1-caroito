5cb87a7bb8fc715a629bad02ccf2a60aea7c9481
caroito <161514036+caroito@users.noreply.github.com>
2024-03-02 19:42:47 +0000

    ejercicio 5 (solo el 1)

---
+++ entrega/g1/ejercicio5.py
@@ -0,0 +1,6 @@
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+edad = input("Ingresa tu edad: ")
+
+print ("Por si te olvidaste, tu nombre es", nombre, apellido, "y tenes", edad, "años.")
