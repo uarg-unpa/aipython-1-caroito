194b1ec84bf877675f335017c3ba782f6a446709
caroito <161514036+caroito@users.noreply.github.com>
2024-03-29 22:02:33 +0000

    aaaa

--- entrega/entrega final/entrega.py
+++ entrega/entrega final/entrega.py
@@ -5,16 +5,17 @@ def menu():
      return mensaje
 
 def lanzar_dado(cant_dados):
+    resultados=[]
     for i in range(cant_dados):
      caras=int(input("Ingrese cuántas caras va a tener el dado: "))
      i = random.randint(1,caras)
      print(f"El resultado del dado de {caras} caras es: {i}")
-
-
+     resultados.append(i) 
+    print(f"Los resultados de los dados fueron: {resultados}") 
 
 
 while True:
      print(menu())
-     cant_dados=int(input("ingrese cuantos dados quiere tirar: "))
+     cant_dados=int(input("Ingrese cuantos dados desea tirar: "))
      lanzar_dado(cant_dados)
      break
